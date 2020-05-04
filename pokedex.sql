e9af7e294fc4b5a70ab9e1fc9d5bff09e5be55b9
Victor Diex Gamar Diaz Urbaneja <sirkennov@outlook.com>
2020-05-04 17:01:56 -0400

    corrigiendo errores minimos

--- pokedex.sql
+++ pokedex.sql
@@ -1,17 +1,17 @@
 /**!
- * @package   	Pokedex
- * @filename  	pokedex.sql
- * @version   	1.0
- * @autor     	Díaz Urbaneja Víctor Eduardo Diex <victor.vector008@gmail.com>
- * @contributor Díaz Urbaneja Víctor Diex Gamar	<Sirkennov@outlook.com>
- * @date      	04.05.2020 14:44:22 -04
+ * @package		Pokedex
+ * @filename	pokedex.sql
+ * @version		1.0
+ * @autor		Díaz Urbaneja Víctor Eduardo Diex <victor.vector008@gmail.com>
+ * @contributor	Díaz Urbaneja Víctor Diex Gamar <Sirkennov@outlook.com>
+ * @date		04.05.2020 14:44:22 -04
  */
 
 pragma foreign_keys = on;
 
 drop table if exists tipos;
 create table tipos (
-	id_tipo integer primary key AUTOINCREMENT,
+	id_tipo	integer primary key AUTOINCREMENT,
 	tipo	varchar
 );
 
@@ -38,7 +38,7 @@ insert into tipos (tipo) values
 
 drop table if exists especies;
 create table especies (
-	id_especie 	integer primary key AUTOINCREMENT,
+	id_especie	integer primary key AUTOINCREMENT,
 	especie		varchar
 );
 
@@ -48,13 +48,13 @@ insert into especies (especie) values
 
 drop table if exists pokemon;
 create table pokemon (
-	id_pokemon 	integer primary key AUTOINCREMENT,
+	id_pokemon	integer primary key AUTOINCREMENT,
 	nombre		varchar,
 	id_especie	integer not null,
 	altura		varchar,
 	peso		varchar,
-	id_tipo		varchar,
+	id_tipo		integer not null,
 	descripcion	varchar,
-	foreign key ( id_especie ) references especies ( id_especie ),
-	foreign key ( id_tipo ) references tipos ( id_tipo )
+	foreign key	( id_especie ) references especies ( id_especie ),
+	foreign key	( id_tipo ) references tipos ( id_tipo )
 );
