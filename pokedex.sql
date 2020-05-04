102798877d0b274ff906c412c1b891e688e2a811
diazvictor <diaz.victor@openmailbox.org>
2020-05-04 15:36:36 -0400

    creado el archivo sql

---
+++ pokedex.sql
@@ -0,0 +1,9 @@
+/**!
+ * @package   Pokedex
+ * @filename  pokedex.sql
+ * @version   1.0
+ * @autor     Díaz Urbaneja Víctor Eduardo Diex <victor.vector008@gmail.com>
+ * @date      04.05.2020 14:44:22 -04
+ */
+
+pragma foreign_keys = on;
