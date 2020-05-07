8494c368885c7e2bc8c9c9c9c376bbd1cf758762
Victor Diex Gamar Diaz Urbaneja <sirkennov@outlook.com>
2020-05-07 17:56:26 -0400

    Actulizando la vista pokedex e indexando el codigo.

--- pokedex.sql
+++ pokedex.sql
@@ -17,15 +17,15 @@ create table habitats (
 
 -- Insertando hábitat de el pokémon
 insert into habitats (habitat) values
-('POKéMON DE PRADERA'),
-('POKéMON DE BOSQUE'),
-('POKéMON AGUA DULCE'),
+('POKéMON DE PRADERA' ),
+('POKéMON DE BOSQUE'  ),
+('POKéMON AGUA DULCE' ),
 ('POKéMON AGUA SALADA'),
-('POKéMON CAVERNA'),
-('POKéMON MONTAÑA'),
-('POKéMON CAMPO'),
-('POKéMON CIUDAD'),
-('POKéMON RAROS');
+('POKéMON CAVERNA'	  ),
+('POKéMON MONTAÑA'	  ),
+('POKéMON CAMPO'	  ),
+('POKéMON CIUDAD'	  ),
+('POKéMON RAROS'	  );
 
 -- Creando la tabla regiones
 create table regiones (
@@ -35,14 +35,14 @@ create table regiones (
 
 -- Insertando datos en la tabla regiones
 insert into regiones (region) values
-('KANTO'),
-('JOHTO'),
-('HOENN'),
-('SINNOH'),
+('KANTO'  ),
+('JOHTO'  ),
+('HOENN'  ),
+('SINNOH' ),
 ('TESELIA'),
-('KALOS'),
-('ALOLA'),
-('GALAR');
+('KALOS'  ),
+('ALOLA'  ),
+('GALAR'  );
 
 -- Creando la tabla tipos
 drop table if exists tipos;
@@ -53,23 +53,23 @@ create table tipos (
 
 -- Insertando los tipos de pokémon
 insert into tipos (tipo) values
-('VOLADOR.'),
-('LUCHA.'),
-('PLANTA.'),
-('FUEGO.'),
-('AGUA.'),
-('ROCA.'),
-('ACERO.'),
-('PSÍQUICO.'),
-('FANTASMA.'),
+('VOLADOR.'	 ),
+('LUCHA.'	 ),
+('PLANTA.'	 ),
+('FUEGO.'	 ),
+('AGUA.'	 ),
+('ROCA.'	 ),
+('ACERO.'	 ),
+('PSÍQUICO.' ),
+('FANTASMA.' ),
 ('SINIESTRO.'),
-('DRAGÓN.'),
-('NORMAL.'),
-('VENENO.'),
-('TIERRA.'),
+('DRAGÓN.'	 ),
+('NORMAL.'	 ),
+('VENENO.'	 ),
+('TIERRA.'	 ),
 ('ELÉCTRICO.'),
-('HIELO.'),
-('BICHO.');
+('HIELO.'	 ),
+('BICHO.'	 );
 
 -- Creando tabla de especies
 drop table if exists especies;
@@ -80,11 +80,11 @@ create table especies (
 
 -- Insertando las especies del pokémon
 insert into especies (especie) values
-('POKéMON SEMILLA'),
-('POKéMON LLAMA'),
-('POKéMON MARISCO'),
-('POKéMON PÁJARO'),
-('POKéMON SOMBRA'),
+('POKéMON SEMILLA'	),
+('POKéMON LLAMA'	),
+('POKéMON MARISCO'	),
+('POKéMON PÁJARO'	),
+('POKéMON SOMBRA'	),
 ('POKéMON RELÁMPAGO');
 
 -- Creando tabla pokemon
@@ -98,9 +98,9 @@ create table pokemon (
 	altura				varchar not null,
 	peso				varchar not null,
 	descripcion			varchar not null,
-	foreign key			( id_especie ) references especies ( id_especie ),
-	foreign key 		(id_habitat	 ) references habitats (id_habitat),
-	foreign key 		(id_region	 ) references regiones (id_region)
+	foreign key			(id_especie  ) references especies (id_especie ),
+	foreign key 		(id_habitat	 ) references habitats (id_habitat ),
+	foreign key 		(id_region	 ) references regiones (id_region  )
 );
 
 -- Insertando datos del pokémon
@@ -138,7 +138,7 @@ insert into pokemon (nombre, id_especie, altura, peso, id_habitat, id_region,  d
 
 create table tipos_pokemon (
 	id_pokemon	integer not null,
-	id_tipo		integer not null,
+	id_tipo		integer,
 	foreign key (id_pokemon) references pokemon (id_pokemon),
 	foreign key (id_tipo) 	 references tipos (id_tipo)
 );
@@ -158,8 +158,10 @@ insert into tipos_pokemon (id_pokemon, id_tipo) values
 -- Creando vista pokédex
 create view pokedex as
 select pokemon.id_pokemon as num, nombre, tipos.tipo,
-especies.especie, pokemon.altura, peso, descripcion
+especies.especie, regiones.region, habitats.habitat,  pokemon.altura, peso, descripcion
 from pokemon
-join especies on ( pokemon.id_especie = especies.id_especie )
-join tipos_pokemon on ( pokemon.id_pokemon = tipos_pokemon.id_pokemon )
-join tipos on ( tipos_pokemon.id_tipo = tipos.id_tipo );
+join especies 	   on ( pokemon.id_especie 	  = especies.id_especie 	 )
+join habitats 	   on ( pokemon.id_habitat 	  = habitats.id_habitat	     )
+join regiones 	   on ( pokemon.id_region  	  = regiones.id_region			 )
+join tipos_pokemon on ( pokemon.id_pokemon	  = tipos_pokemon.id_pokemon )
+join tipos 		   on ( tipos_pokemon.id_tipo = tipos.id_tipo			 );
